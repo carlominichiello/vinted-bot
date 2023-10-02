7e9246edbe920d9330ad8f3b11e5bd3b6df23f9d
estebanthi <esteban.thilliez@gmail.com>
2023-10-02 09:46:55 +0200

    Refactor

--- src/scraping/monitor.py
+++ src/scraping/monitor.py
@@ -26,7 +26,7 @@ class Monitor:
 
         for webhook, value in watch_webhooks.items():
             try:
-                self._process_webhook(webhook, value, bot_service, database)
+                self._process_url(value['url'], database, webhook=webhook, bot_service=bot_service)
             except requests.exceptions.HTTPError as e:
                 logger.error(f"Error while scraping {value['url']}: {e}")
                 bot_service.on_error(e)
@@ -39,9 +39,10 @@ class Monitor:
         background_scraping_webhooks = bot_service.get_background_scraping_webhooks()
         for webhook, value in background_scraping_webhooks.items():
             thread_id = random.randint(0, 100000)
-            self._start_background_scrape_thread(webhook, value, bot_service, database, thread_id)
+            self._start_background_scrape_thread(value['url'], database, thread_id, webhook, bot_service)
 
-
+    def run_random_scraping(self, database):
+        pass
 
     def _wait(self):
         time_start = time.time()
@@ -50,19 +51,19 @@ class Monitor:
             if time.time() - time_start > self._config["recheck_interval"]:
                 break
 
-    def _process_webhook(self, webhook, value, bot_service, database, page_start=1, page_end=None):
-        params = self._query_generator.get_query(value["url"])
+    def _process_url(self, url, database, page_start=1, page_end=None, webhook=None, bot_service=None):
+        params = self._query_generator.get_query(url)
 
         items_ids = self._scraper.scrape_items(**params, start_page=page_start, end_page=page_end)
         if not items_ids:
-            raise requests.exceptions.HTTPError(f"Items not found for {value['url']}")
+            raise requests.exceptions.HTTPError(f"Items not found for {url}")
 
         new_items_ids = database.get_no_dupes(database.items, items_ids)
-        logger.info(f"Found {len(new_items_ids)} new items for {value['url']} (page start: {page_start}, page end: {page_end})")
+        logger.info(f"Found {len(new_items_ids)} new items for {url} (page start: {page_start}, page end: {page_end})")
         for item_id in new_items_ids:
-            self._process_item(item_id, webhook, bot_service, database)
+            self._process_item(item_id, database, webhook, bot_service)
 
-    def _process_item(self, item_id, webhook, bot_service, database):
+    def _process_item(self, item_id, database, webhook=None, bot_service=None):
         try:
             json_item, json_user = self._scraper.scrape_item(item_id)
 
@@ -72,23 +73,31 @@ class Monitor:
             self._on_data(json_item, database.items, database)
             self._on_data(json_user, database.users, database)
 
-            bot_service.process_item(json_item, json_user, webhook)
+            if webhook and bot_service:
+                bot_service.process_item(json_item, json_user, webhook)
 
             logger.debug(f"Sleeping for {self._config['request_interval']} seconds")
             time.sleep(self._config["request_interval"])
 
         except src.exceptions.RetryException as e:
             logger.error(f"Error while scraping {item_id}, retrying: {e}")
-            bot_service.on_error(e)
-            self._process_item(item_id, webhook, bot_service, database)
+
+            if bot_service:
+                bot_service.on_error(e)
+
+            self._process_item(item_id, database, webhook, bot_service)
 
         except requests.exceptions.HTTPError as e:
             logger.error(f"Error while scraping {item_id}: {e}")
-            bot_service.on_error(e)
+
+            if bot_service:
+                bot_service.on_error(e)
         
         except Exception as e:
             logger.error(f"Error while scraping {item_id}: {e}")
-            bot_service.on_error(e)
+
+            if bot_service:
+                bot_service.on_error(e)
 
     def _on_data(self, json_data, collection, database):
         return (
@@ -105,23 +114,26 @@ class Monitor:
         database.insert(json_data, collection)
         return False
 
-    def _start_background_scrape_thread(self, webhook, value, bot_service, database, thread_id):
+    def _start_background_scrape_thread(self, url, database, thread_id, webhook=None, bot_service=None):
         self._threads.append(thread_id)
-        self._background_scrape_thread = threading.Thread(target=self._background_scrape, args=(webhook, value, bot_service, database, thread_id))
+        self._background_scrape_thread = threading.Thread(target=self._background_scrape, args=(url, database, thread_id, webhook, bot_service))
         self._background_scrape_thread.start()
 
     def _stop_background_scrape_thread(self, thread_id):
         self._threads.remove(thread_id)
 
-    def _background_scrape(self, webhook, value, bot_service, database, thread_id):
-        logger.info(f"Starting background scraping for {value['url']}")
+    def _background_scrape(self, url, database, thread_id, webhook=None, bot_service=None):
+        logger.info(f"Starting background scraping for {url}")
         logger.debug(f"Thread id: {thread_id}")
         page_start = 1
         while thread_id in self._threads:
             try:
-                self._process_webhook(webhook, value, bot_service, database, page_start)
+                self._process_url(url, database, page_start, webhook=webhook, bot_service=bot_service)
             except requests.exceptions.HTTPError as e:
-                logger.error(f"Error while scraping {value['url']}: {e}")
-                bot_service.on_error(e)
+                logger.error(f"Error while scraping {url}: {e}")
+
+                if bot_service:
+                    bot_service.on_error(e)
+
                 self._stop_background_scrape_thread(thread_id)
             page_start += 1
